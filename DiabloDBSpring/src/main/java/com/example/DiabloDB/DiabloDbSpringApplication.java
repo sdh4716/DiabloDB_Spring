791cf5bf1dc5fdbd96c02bcd66917add2f53bb40
sdh4716 <sdh4716@naver.com>
2021-12-03 10:01:37 +0900

    DiabloDB Spring

---
+++ DiabloDBSpring/src/main/java/com/example/DiabloDB/DiabloDbSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.DiabloDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabloDbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabloDbSpringApplication.class, args);
+	}
+
+}
---
+++ DiabloDBSpring/src/test/java/com/example/DiabloDB/DiabloDbSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiabloDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabloDbSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
